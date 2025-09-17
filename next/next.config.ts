b72cf418318ec6043228035673275ed6ce6ffee0
RosieOh <dhxogns920@gmail.com>
2025-09-17 16:55:31 +0900

    FEAT : lh3 이미지 콘텐츠 프로토콜 설정 추가

--- next/next.config.ts
+++ next/next.config.ts
@@ -1,7 +1,16 @@
 import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+};
 
 const withPWAInit = withPWA({
   dest: "public",
