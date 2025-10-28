a5143dc53ee955a33c87a41ccf2208e5bfedbee9
mstagon <devcms21@gmail.com>
2025-10-28 11:04:58 +0900

    feat: 목데이터 비활성화

--- next/lib/config.ts
+++ next/lib/config.ts
@@ -1,5 +1,5 @@
 // 환경 토글 및 공통 설정
-export const USE_MOCK = true; // 항상 Mock 데이터 사용
+export const USE_MOCK = false; // 항상 Mock 데이터 사용
 
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
